9a30acbfc45393d07397af052c3f1d40a6c99c56
LucienChotard <chotardlucien@gmail.com>
2019-12-16 16:19:40 +0100

    timeline

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@ const landing = document.querySelector('#landing')
 const startBtn = document.querySelector('#start-btn')
 const wave = document.querySelector('#wave')
 const backWave = document.querySelector('#back-wave')
+const player = document.querySelector('#player')
+const playBtn = document.querySelector('#play-btn')
+
+class Video {
+  constructor(videoId,progressId){
+    this.video = document.querySelector(videoId)
+    this.progress = document.querySelector(progressId)
+  }
+}
+let vidCity = new Video('#vid-city',"#progress-city")
+let vidOcean = new Video('#vid-ocean',"#progress-ocean")
+let vidRewind = new Video('#vid-rewind',"#progress-rewind")
+
+let globalVid = [vidCity,vidOcean,vidRewind]
+
 
 startBtn.addEventListener('click',(e)=>{
   wave.classList.remove('wave-move')
@@ -13,5 +28,34 @@ startBtn.addEventListener('click',(e)=>{
   }, 500);
   setTimeout(function(){
     landing.style.display="none"
+    player.style.display="block"
+    player.style.opacity="0"
+    player.classList.add('fade-in')
+    vidCity.video.play()
   }, 2500);
 })
+
+playBtn.addEventListener('click',(e)=>{
+  if(vidCity.video.paused){
+    playAll()
+  }
+  else{
+    pauseAll()
+  }
+})
+
+function playAll(){
+  for(i of globalVid){
+    i.video.play()
+  }
+  playBtn.classList.toggle("icofont-ui-play")
+  playBtn.classList.toggle("icofont-ui-pause")
+}
+
+function pauseAll(){
+  for(i of globalVid){
+    i.video.pause()
+  }
+  playBtn.classList.toggle("icofont-ui-pause")
+  playBtn.classList.toggle("icofont-ui-play")
+}
